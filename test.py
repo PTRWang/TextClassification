fe90da1d5a6648e83792e5429ed5ba5c76c3ae40
PTRWang <p59wang@uwaterloo.ca>
2015-08-22 11:12:27 -0700

    first commit

---
+++ test.py
@@ -0,0 +1,12 @@
+import os
+import re
+
+def getNames():
+	filename = "names/yob"
+	for i in range(1880,2015):
+		text_file = open()
+	for line in 
+	print lines
+
+if __name__ == "__main__":
+	getNames()
